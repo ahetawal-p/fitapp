3dc962c16a5d70806a369be2c4c64829c792ccf6
Beck Chen <bcfchen@gmail.com>
2015-06-05 22:26:53 -0700

    increasing raw activity threshold and grouped activity threshold to increase performance

--- www/app/support/utils/utilsConstants.js
+++ www/app/support/utils/utilsConstants.js
@@ -3,6 +3,6 @@ angular
     .module('app.utils')
     .constant("UTILS_CONSTANTS", {
         "GROUPED_DISTANCE_THRESHOLD" : 0.1,
-        "RAW_DISTANCE_THRESHOLD" : 0.05,
-        "RAW_DISTANCE_THRESHOLD_OS81" : 0.001
+        "RAW_DISTANCE_THRESHOLD" : 0.08,
+        "RAW_DISTANCE_THRESHOLD_OS81" : 0.01
     });
